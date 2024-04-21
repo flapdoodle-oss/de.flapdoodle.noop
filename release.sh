75b47e443e25d14fb26b3e9b34b6d5ea21620351
Michael Mosmann <michael@mosmann.de>
2024-04-21 11:44:58 +0200

    disable test in release was the cause for missing gpg sign

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./mvnw release:clean -DskipPitest -DskipTests -Darguments=-DskipTests
-./mvnw release:prepare -DskipPitest -DskipTests -Darguments=-DskipTests
-./mvnw release:perform -DskipPitest -DskipTests -Darguments=-DskipTests
+./mvnw release:clean -DskipPitest -DskipTests
+./mvnw release:prepare -DskipPitest -DskipTests
+./mvnw release:perform -DskipPitest -DskipTests
 
